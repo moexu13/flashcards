6a47e9b827e89d5daed309951b8497792f9122d7
Melissa Albarella <moexu13@gmail.com>
2021-03-03 14:58:04 -0800

    added styling to header

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="jumbotron bg-dark">
+    <header className="jumbotron header">
       <div className="container text-white">
         <h1 className="display-4">Flashcard-o-matic</h1>
-        <p className="lead">Discover The Flashcard Difference.</p>
+        <p className="header__lead">Discover The Flashcard Difference.</p>
       </div>
     </header>
   );
